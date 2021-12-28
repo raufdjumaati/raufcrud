87ba18e1b851aefc63a71edc27be0284e256ea56
raufdjumaati <raufmuhardjumaati@gmail.com>
2021-12-28 17:23:00 +0700

    Tampil Data

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>UAS CRUD</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+<div class =<"container"> 
+<h1 class="text-center">UAS CRUD</h1>
+<h2 class="text-center">@raufdjumaati</h2>
+
+    <!-- Awal card form -->
+    <div class="card mt-3">
+    <div class="card-header bg-primary text-white">
+        form Data Mahasiswa
+    </div>
+    <div class="card-body">
+        <form method="post" action="">
+            <div class="form-group">
+                <label>Nim</label>
+                <input type="text" name="tnim" class="form-control" placeholder="input Nim anda disini!" required>
+                <div class="form-group">
+                <label>Nama</label>
+                <input type="text" name="tnim" class="form-control" placeholder="Masukan Nama Lengkap Anda" required>
+                <div class="form-group">
+                <label>Tanggal Lahir</label>
+                <input type="text" name="tnim" class="form-control" placeholder="Tempat Tanggal lahir" required>
+                <div class="form-group">
+                <label>Agama</label>
+                <input type="text" name="tnim" class="form-control" placeholder="Masukan Kepercayaan Anda" required>
+                <div class="form-group">
+                <label>Alamat Tinggal</label>
+                <input type="text" name="tnim" class="form-control" placeholder="Masukan Alamat Tinggal Sekarang" required>
+                <div class="form-group">
+                <label>Umur</label>
+                <input type="text" name="tnim" class="form-control" placeholder="...Tahun" required>
+                <div class="form-group">
+                <label>Jurusan</label>
+                <input type="text" name="tnim" class="form-control" placeholder="Masukan Jurusaan Yang Anda Ambil" required>
+                <div class="form-group">
+                <label>Prodi</label>
+                <input type="text" name="tnim" class="form-control" placeholder="Masukan Yang Anda Ambil" required>
+                <div class="form-group">
+                <label>Jenis Kelamin</label>
+                <input type="text" name="tnim" class="form-control" placeholder= "L/P" required>
+                <div class="form-group">
+                <label>Angkatan</label>
+                <input type="text" name="tnim" class="form-control" placeholder="Tahun Masuk Perguruan Tinggi" required>
+    <!-- Akhir card form -->
+<script type="text/javacript"src="js/bootstrap.min.js"></script>
+</body>
+</html>
